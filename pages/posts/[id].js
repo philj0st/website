1a4e72f3f348da7897e893577ad4d4956ca61a3f
jostphi <phil@jost.rocks>
2021-05-19 15:11:52 +0200

    fix: linux build failing

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 
 import Layout from "../../components/layout";
 import Date from "../../components/date";
